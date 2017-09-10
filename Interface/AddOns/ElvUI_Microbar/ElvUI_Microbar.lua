1fedbe2d4ef96a2e814c5040f7932925c482d384
Marelle <danvdh@gmail.com>
2017-09-10 00:20:00 -0400

    Addons: Update ElvUI microbar

--- Interface/AddOns/ElvUI_Microbar/ElvUI_Microbar.lua
+++ Interface/AddOns/ElvUI_Microbar/ElvUI_Microbar.lua
@@ -1,28 +1,12 @@
-﻿-------------------------------------------------
---
--- ElvUI Microbar Enhancement by Darth Predator
--- Дартпредатор - Вечная Песня (Eversong) RU
---
--------------------------------------------------
---
--- Thanks to / Благодарности:
--- Elv and ElvUI community
--- Blazeflack for helping with option storage and profile changing
---
--------------------------------------------------
---
--- Usage / Использование:
--- Just install and configure for yourself
--- Устанавливаем, настраиваем и получаем профит
---
--------------------------------------------------
-
-local E, L, V, P, G, _ =  unpack(ElvUI);
-local AB = E:GetModule("ActionBars");
+﻿local E, L, V, P, G =  unpack(ElvUI)
+local AB = E:GetModule("ActionBars")
 local EP = LibStub("LibElvUIPlugin-1.0")
 local S = E:GetModule("Skins")
 local addon = "ElvUI_MicroBar"
 local UB
+
+local LFG_TITLE = LFG_TITLE
+local LFM_TITLE = LFM_TITLE
 
 P.actionbar.microbar.scale = 1
 P.actionbar.microbar.symbolic = false
@@ -39,7 +23,7 @@ local MICRO_BUTTONS = {
 	"LFGMicroButton",
 	"MainMenuMicroButton",
 	"HelpMicroButton"
-};
+}
 
 local Sbuttons = {}
 
@@ -52,16 +36,16 @@ function AB:GetOptions()
 		isPercent = true,
 		min = 0.3, max = 2, step = 0.01,
 		get = function(info) return AB.db.microbar.scale end,
-		set = function(info, value) AB.db.microbar.scale = value; AB:UpdateMicroPositionDimensions(); end
-	};
+		set = function(info, value) AB.db.microbar.scale = value AB:UpdateMicroPositionDimensions() end
+	}
 	E.Options.args.actionbar.args.microbar.args.backdrop = {
 		order = 6,
 		type = "toggle",
 		name = L["Backdrop"],
 		disabled = function() return not AB.db.microbar.enabled end,
 		get = function(info) return AB.db.microbar.backdrop end,
-		set = function(info, value) AB.db.microbar.backdrop = value; AB:UpdateMicroPositionDimensions(); end
-	};
+		set = function(info, value) AB.db.microbar.backdrop = value AB:UpdateMicroPositionDimensions() end
+	}
 	E.Options.args.actionbar.args.microbar.args.symbolic = {
 		order = 7,
 		type = "toggle",
@@ -69,246 +53,246 @@ function AB:GetOptions()
 		desc = L["Replace icons with just letters.\n|cffFF0000Warning:|r this will disable original Blizzard's tooltips for microbar."],
 		disabled = function() return not AB.db.microbar.enabled end,
 		get = function(info) return AB.db.microbar.symbolic end,
-		set = function(info, value) AB.db.microbar.symbolic = value; AB:MenuShow(); end
-	};
+		set = function(info, value) AB.db.microbar.symbolic = value AB:MenuShow() end
+	}
 	E.Options.args.actionbar.args.microbar.args.color = {
 		order = 8,
 		type = "color",
 		name = L["Text Color"],
 		get = function(info)
-			local t = AB.db.microbar.colorS;
-			local d = P.actionbar.microbar.colorS;
-			return t.r, t.g, t.b, d.r, d.g, d.b;
+			local t = AB.db.microbar.colorS
+			local d = P.actionbar.microbar.colorS
+			return t.r, t.g, t.b, d.r, d.g, d.b
 		end,
 		set = function(info, r, g, b)
-			local t = AB.db.microbar.colorS;
-			t.r, t.g, t.b = r, g, b;
-			AB:SetSymbloColor();
+			local t = AB.db.microbar.colorS
+			t.r, t.g, t.b = r, g, b
+			AB:SetSymbloColor()
 		end,
-		disabled = function() return not AB.db.microbar.enabled or AB.db.microbar.classColor; end
-	};
+		disabled = function() return not AB.db.microbar.enabled or AB.db.microbar.classColor end
+	}
 	E.Options.args.actionbar.args.microbar.args.classColor = {
 		order = 9,
 		type = "toggle",
 		name = CLASS,
-		disabled = function() return not AB.db.microbar.enabled; end,
-		get = function(info) return AB.db.microbar.classColor; end,
-		set = function(info, value) AB.db.microbar.classColor = value; AB:SetSymbloColor(); end
-	};
+		disabled = function() return not AB.db.microbar.enabled end,
+		get = function(info) return AB.db.microbar.classColor end,
+		set = function(info, value) AB.db.microbar.classColor = value AB:SetSymbloColor() end
+	}
 end
 
 function AB:MicroScale()
-	ElvUI_MicroBar.mover:SetWidth(AB.MicroWidth*AB.db.microbar.scale);
-	ElvUI_MicroBar.mover:SetHeight(AB.MicroHeight*AB.db.microbar.scale);
-	ElvUI_MicroBar:SetScale(AB.db.microbar.scale);
-	ElvUI_MicroBarS:SetScale(AB.db.microbar.scale);
+	ElvUI_MicroBar.mover:SetWidth(AB.MicroWidth*AB.db.microbar.scale)
+	ElvUI_MicroBar.mover:SetHeight(AB.MicroHeight*AB.db.microbar.scale)
+	ElvUI_MicroBar:SetScale(AB.db.microbar.scale)
+	ElvUI_MicroBarS:SetScale(AB.db.microbar.scale)
 end
 
 E.UpdateAllMB = E.UpdateAll
 function E:UpdateAll()
-    E.UpdateAllMB(self);
-	AB:MicroScale();
-	AB:MenuShow();
+    E.UpdateAllMB(self)
+	AB:MicroScale()
+	AB:MenuShow()
 end
 
 local function Letter_OnEnter()
 	if(AB.db.microbar.mouseover) then
-		E:UIFrameFadeIn(ElvUI_MicroBarS, 0.2, ElvUI_MicroBarS:GetAlpha(), AB.db.microbar.alpha);
+		E:UIFrameFadeIn(ElvUI_MicroBarS, 0.2, ElvUI_MicroBarS:GetAlpha(), AB.db.microbar.alpha)
 	end
 end
 
 local function Letter_OnLeave()
 	if(AB.db.microbar.mouseover) then
-		E:UIFrameFadeOut(ElvUI_MicroBarS, 0.2, ElvUI_MicroBarS:GetAlpha(), 0);
+		E:UIFrameFadeOut(ElvUI_MicroBarS, 0.2, ElvUI_MicroBarS:GetAlpha(), 0)
 	end
 end
 
 function AB:CreateSymbolButton(name, text, tooltip, click)
-	local button = CreateFrame("Button", name, ElvUI_MicroBarS);
-	button:SetScript("OnClick", click);
+	local button = CreateFrame("Button", name, ElvUI_MicroBarS)
+	button:SetScript("OnClick", click)
 	if(tooltip) then
 		button:SetScript("OnEnter", function(self)
-			Letter_OnEnter();
-			GameTooltip:SetOwner(self);
-			GameTooltip:AddLine(tooltip, 1, 1, 1, 1, 1, 1);
-			GameTooltip:Show();
-		end);
+			Letter_OnEnter()
+			GameTooltip:SetOwner(self)
+			GameTooltip:AddLine(tooltip, 1, 1, 1, 1, 1, 1)
+			GameTooltip:Show()
+		end)
 		button:SetScript("OnLeave", function(self)
-			Letter_OnLeave();
-			GameTooltip:Hide();
-		end);
+			Letter_OnLeave()
+			GameTooltip:Hide()
+		end)
 	else
-		button:HookScript("OnEnter", Letter_OnEnter);
-		button:HookScript("OnEnter", Letter_OnLeave);
+		button:HookScript("OnEnter", Letter_OnEnter)
+		button:HookScript("OnEnter", Letter_OnLeave)
 	end
 
-	S:HandleButton(button);
+	S:HandleButton(button)
 
 	if(text) then
-		button.text = button:CreateFontString(nil,"OVERLAY",button);
-		button.text:FontTemplate();
-		button.text:SetPoint("CENTER", button, "CENTER", 0, -1);
-		button.text:SetJustifyH("CENTER");
-		button.text:SetText(text);
-		button:SetFontString(button.text);
+		button.text = button:CreateFontString(nil,"OVERLAY",button)
+		button.text:FontTemplate()
+		button.text:SetPoint("CENTER", button, "CENTER", 0, -1)
+		button.text:SetJustifyH("CENTER")
+		button.text:SetText(text)
+		button:SetFontString(button.text)
 	end
 
-	tinsert(Sbuttons, button);
+	tinsert(Sbuttons, button)
 end
 
 function AB:SetSymbloColor()
-	local color = AB.db.microbar.classColor and (E.myclass == "PRIEST" and E.PriestColors or (CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass])) or AB.db.microbar.colorS;
+	local color = AB.db.microbar.classColor and (E.myclass == "PRIEST" and E.PriestColors or (CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[E.myclass] or RAID_CLASS_COLORS[E.myclass])) or AB.db.microbar.colorS
 	for i = 1, #Sbuttons do
-		Sbuttons[i].text:SetTextColor(color.r, color.g, color.b);
+		Sbuttons[i].text:SetTextColor(color.r, color.g, color.b)
 	end
 end
 
 function AB:SetupSymbolBar()
-	local frame = CreateFrame("Frame", "ElvUI_MicroBarS", E.UIParent);
-	frame:SetPoint("CENTER", ElvUI_MicroBar, 0, 0);
-	frame:EnableMouse(true);
-	frame:SetScript("OnEnter", Letter_OnEnter);
-	frame:SetScript("OnLeave", Letter_OnLeave);
+	local frame = CreateFrame("Frame", "ElvUI_MicroBarS", E.UIParent)
+	frame:SetPoint("CENTER", ElvUI_MicroBar, 0, 0)
+	frame:EnableMouse(true)
+	frame:SetScript("OnEnter", Letter_OnEnter)
+	frame:SetScript("OnLeave", Letter_OnLeave)
 
 	AB:CreateSymbolButton("EMB_Character", "C", MicroButtonTooltipText(CHARACTER_INFO, "TOGGLECHARACTER0"), function()
 		if(CharacterFrame:IsShown()) then
-			HideUIPanel(CharacterFrame);
+			HideUIPanel(CharacterFrame)
 		else
-			ShowUIPanel(CharacterFrame);
+			ShowUIPanel(CharacterFrame)
 		end
-	end);
+	end)
 	AB:CreateSymbolButton("EMB_Spellbook", "S", MicroButtonTooltipText(SPELLBOOK_ABILITIES_BUTTON, "TOGGLESPELLBOOK"), function()
 		if(SpellBookFrame:IsShown()) then
-			HideUIPanel(SpellBookFrame);
+			HideUIPanel(SpellBookFrame)
 		else
-			ShowUIPanel(SpellBookFrame);
+			ShowUIPanel(SpellBookFrame)
 		end
-	end);
+	end)
 	AB:CreateSymbolButton("EMB_Talents", "T", MicroButtonTooltipText(TALENTS_BUTTON, "TOGGLETALENTS"), function()
 		if(UnitLevel("player") >= 10) then
 			if(PlayerTalentFrame) then
 				if(PlayerTalentFrame:IsShown()) then
-					HideUIPanel(PlayerTalentFrame);
+					HideUIPanel(PlayerTalentFrame)
 				else
-					ShowUIPanel(PlayerTalentFrame);
+					ShowUIPanel(PlayerTalentFrame)
 				end
 			else
-				LoadAddOn("Blizzard_TalentUI");
-				ShowUIPanel(PlayerTalentFrame);
+				LoadAddOn("Blizzard_TalentUI")
+				ShowUIPanel(PlayerTalentFrame)
 			end
 		end
 	end)
 	AB:CreateSymbolButton("EMB_Quest", "Q", MicroButtonTooltipText(QUESTLOG_BUTTON, "TOGGLEQUESTLOG"), function()
 		if(QuestLogFrame:IsShown()) then
-			HideUIPanel(QuestLogFrame);
+			HideUIPanel(QuestLogFrame)
 		else
-			ShowUIPanel(QuestLogFrame);
+			ShowUIPanel(QuestLogFrame)
 		end
-	end);
-	AB:CreateSymbolButton("EMB_Socials", "F", MicroButtonTooltipText(SOCIAL_BUTTON, "TOGGLESOCIAL"), function() ToggleFriendsFrame(); end);
-	AB:CreateSymbolButton("EMB_LFG", "D", MicroButtonTooltipText(L["Looking For Group/Looking For More"], "TOGGLELFGPARENT"), function() ToggleLFGParentFrame(); end);
+	end)
+	AB:CreateSymbolButton("EMB_Socials", "F", MicroButtonTooltipText(SOCIAL_BUTTON, "TOGGLESOCIAL"), function() ToggleFriendsFrame() end)
+	AB:CreateSymbolButton("EMB_LFG", "D", MicroButtonTooltipText(LFG_TITLE.."/"..LFM_TITLE, "TOGGLELFGPARENT"), function() ToggleLFGParentFrame() end)
 	AB:CreateSymbolButton("EMB_MenuSys", "M", MicroButtonTooltipText(MAINMENU_BUTTON, "TOGGLEGAMEMENU"), function()
 		if(GameMenuFrame:IsShown()) then
-			PlaySound("igMainMenuQuit");
-			HideUIPanel(GameMenuFrame);
+			PlaySound("igMainMenuQuit")
+			HideUIPanel(GameMenuFrame)
 		else
-			PlaySound("igMainMenuOpen");
-			ShowUIPanel(GameMenuFrame);
+			PlaySound("igMainMenuOpen")
+			ShowUIPanel(GameMenuFrame)
 		end
-	end);
-	AB:CreateSymbolButton("EMB_Help", "?", HELP_BUTTON, function() ToggleHelpFrame(); end);
+	end)
+	AB:CreateSymbolButton("EMB_Help", "?", HELP_BUTTON, function() ToggleHelpFrame() end)
 
-	AB:UpdateMicroPositionDimensions();
+	AB:UpdateMicroPositionDimensions()
 end
 
 function AB:UpdateMicroPositionDimensions()
-	if(not ElvUI_MicroBar) then return; end
+	if(not ElvUI_MicroBar) then return end
 
-	local numRows = 1;
+	local numRows = 1
 	for i = 1, #MICRO_BUTTONS do
-		local button = _G[MICRO_BUTTONS[i]];
-		local prevButton = _G[MICRO_BUTTONS[i-1]] or ElvUI_MicroBar;
-		local lastColumnButton = _G[MICRO_BUTTONS[i-self.db.microbar.buttonsPerRow]];
+		local button = _G[MICRO_BUTTONS[i]]
+		local prevButton = _G[MICRO_BUTTONS[i-1]] or ElvUI_MicroBar
+		local lastColumnButton = _G[MICRO_BUTTONS[i-self.db.microbar.buttonsPerRow]]
 
-		button:ClearAllPoints();
+		button:ClearAllPoints()
 		if(prevButton == ElvUI_MicroBar) then
-			button:SetPoint("TOPLEFT", prevButton, "TOPLEFT", -2 + E.Border, 28 - E.Border);
+			button:SetPoint("TOPLEFT", prevButton, "TOPLEFT", -2 + E.Border, 28 - E.Border)
 		elseif((i - 1) % self.db.microbar.buttonsPerRow == 0) then
-			button:Point("TOP", lastColumnButton, "BOTTOM", 0, 28 - self.db.microbar.yOffset);
-			numRows = numRows + 1;
+			button:Point("TOP", lastColumnButton, "BOTTOM", 0, 28 - self.db.microbar.yOffset)
+			numRows = numRows + 1
 		else
-			button:Point("LEFT", prevButton, "RIGHT", - 4 + self.db.microbar.xOffset, 0);
+			button:Point("LEFT", prevButton, "RIGHT", - 4 + self.db.microbar.xOffset, 0)
 		end
 	end
 
 	if(AB.db.microbar.mouseover) then
-		ElvUI_MicroBar:SetAlpha(0);
+		ElvUI_MicroBar:SetAlpha(0)
 	else
-		ElvUI_MicroBar:SetAlpha(self.db.microbar.alpha);
+		ElvUI_MicroBar:SetAlpha(self.db.microbar.alpha)
 	end
 
-	AB.MicroWidth = ((CharacterMicroButton:GetWidth() - 4) * self.db.microbar.buttonsPerRow) + (self.db.microbar.xOffset * (self.db.microbar.buttonsPerRow-1)) + E.Border*2;
-	AB.MicroHeight = ((CharacterMicroButton:GetHeight() - 28) * numRows) + (self.db.microbar.yOffset * (numRows-1)) + E.Border*2;
+	AB.MicroWidth = ((CharacterMicroButton:GetWidth() - 4) * self.db.microbar.buttonsPerRow) + (self.db.microbar.xOffset * (self.db.microbar.buttonsPerRow-1)) + E.Border*2
+	AB.MicroHeight = ((CharacterMicroButton:GetHeight() - 28) * numRows) + (self.db.microbar.yOffset * (numRows-1)) + E.Border*2
 
-	ElvUI_MicroBar:SetWidth(AB.MicroWidth);
-	ElvUI_MicroBar:SetHeight(AB.MicroHeight);
+	ElvUI_MicroBar:SetWidth(AB.MicroWidth)
+	ElvUI_MicroBar:SetHeight(AB.MicroHeight)
 
 	if(not ElvUI_MicroBar.backdrop) then
-		ElvUI_MicroBar:CreateBackdrop("Transparent");
+		ElvUI_MicroBar:CreateBackdrop("Transparent")
 	end
 
 	if(self.db.microbar.enabled) then
-		ElvUI_MicroBar:Show();
+		ElvUI_MicroBar:Show()
 	else
-		ElvUI_MicroBar:Hide();
+		ElvUI_MicroBar:Hide()
 	end
 
-	if(not Sbuttons[1]) then return; end
-	AB:MenuShow();
-	local numRowsS = 1;
+	if(not Sbuttons[1]) then return end
+	AB:MenuShow()
+	local numRowsS = 1
 	for i=1, #Sbuttons do
-		local button = Sbuttons[i];
-		local prevButton = Sbuttons[i-1] or ElvUI_MicroBarS;
-		local lastColumnButton = Sbuttons[i-self.db.microbar.buttonsPerRow];
+		local button = Sbuttons[i]
+		local prevButton = Sbuttons[i-1] or ElvUI_MicroBarS
+		local lastColumnButton = Sbuttons[i-self.db.microbar.buttonsPerRow]
 		button:Width(28 - 4)
-		button:Height(58 - 28);
+		button:Height(58 - 28)
 
-		button:ClearAllPoints();
+		button:ClearAllPoints()
 		if(prevButton == ElvUI_MicroBarS) then
-			button:SetPoint("TOPLEFT", prevButton, "TOPLEFT", E.Border, -E.Border);
+			button:SetPoint("TOPLEFT", prevButton, "TOPLEFT", E.Border, -E.Border)
 		elseif((i - 1) % self.db.microbar.buttonsPerRow == 0) then
-			button:Point("TOP", lastColumnButton, "BOTTOM", 0, -self.db.microbar.yOffset);
-			numRowsS = numRowsS + 1;
+			button:Point("TOP", lastColumnButton, "BOTTOM", 0, -self.db.microbar.yOffset)
+			numRowsS = numRowsS + 1
 		else
-			button:Point("LEFT", prevButton, "RIGHT", self.db.microbar.xOffset, 0);
+			button:Point("LEFT", prevButton, "RIGHT", self.db.microbar.xOffset, 0)
 		end
 
-		prevButton = button;
+		prevButton = button
 	end
 
-	ElvUI_MicroBarS:SetWidth(AB.MicroWidth);
-	ElvUI_MicroBarS:SetHeight(AB.MicroHeight);
+	ElvUI_MicroBarS:SetWidth(AB.MicroWidth)
+	ElvUI_MicroBarS:SetHeight(AB.MicroHeight)
 
 	if(not ElvUI_MicroBarS.backdrop) then
-		ElvUI_MicroBarS:CreateBackdrop("Transparent");
+		ElvUI_MicroBarS:CreateBackdrop("Transparent")
 	end
 
 	if(AB.db.microbar.backdrop) then
-		ElvUI_MicroBar.backdrop:Show();
-		ElvUI_MicroBarS.backdrop:Show();
+		ElvUI_MicroBar.backdrop:Show()
+		ElvUI_MicroBarS.backdrop:Show()
 	else
-		ElvUI_MicroBar.backdrop:Hide();
-		ElvUI_MicroBarS.backdrop:Hide();
+		ElvUI_MicroBar.backdrop:Hide()
+		ElvUI_MicroBarS.backdrop:Hide()
 	end
 
 	if(AB.db.microbar.mouseover) then
-		ElvUI_MicroBarS:SetAlpha(0);
+		ElvUI_MicroBarS:SetAlpha(0)
 	elseif(not AB.db.microbar.mouseover and  AB.db.microbar.symbolic) then
-		ElvUI_MicroBarS:SetAlpha(AB.db.microbar.alpha);
+		ElvUI_MicroBarS:SetAlpha(AB.db.microbar.alpha)
 	end
 
-	AB:MicroScale();
-	AB:SetSymbloColor();
+	AB:MicroScale()
+	AB:SetSymbloColor()
 end
 
 function AB:MenuShow()
@@ -331,24 +315,24 @@ function AB:MenuShow()
 end
 
 -- function AB:CreateUIButton()
-	-- UB:CreateDropdownButton(true, "Addon", "Microbar", L["Micro Bar"], L["Micro Bar"], nil, function() E:ToggleConfig(); LibStub("AceConfigDialog-3.0"):SelectGroup("ElvUI", "actionbar", "microbar") end)
+	-- UB:CreateDropdownButton(true, "Addon", "Microbar", L["Micro Bar"], L["Micro Bar"], nil, function() E:ToggleConfig() LibStub("AceConfigDialog-3.0"):SelectGroup("ElvUI", "actionbar", "microbar") end)
 -- end
 
 function AB:EnhancementInit()
-	EP:RegisterPlugin(addon, AB.GetOptions);
-	AB:SetupSymbolBar();
-	AB:MenuShow();
+	EP:RegisterPlugin(addon, AB.GetOptions)
+	AB:SetupSymbolBar()
+	AB:MenuShow()
 
 	-- if not IsAddOnLoaded("ElvUI_SLE") then return end
-	-- UB = E:GetModule("SLE_UIButtons");
+	-- UB = E:GetModule("SLE_UIButtons")
 	-- hooksecurefunc(UB, "InsertButtons", AB.CreateUIButton)
 end
 
 hooksecurefunc(AB, "SetupMicroBar", AB.EnhancementInit)
 
-local f = CreateFrame("Frame");
-f:RegisterEvent("PLAYER_ENTERING_WORLD");
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function(self)
-	self:UnregisterEvent("PLAYER_ENTERING_WORLD");
-	AB:MicroScale();
-end);
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	AB:MicroScale()
+end)
--- Interface/AddOns/ElvUI_Microbar/locales/english.lua
+++ Interface/AddOns/ElvUI_Microbar/locales/english.lua
@@ -3,7 +3,7 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
 local L = AceLocale:NewLocale("ElvUI", "enUS", true);
 if not L then return; end
 
-L["Set Scale"] = "Set Scale"
-L["Sets Scale of the Micro Bar"] = "Sets Scale of the Micro Bar"
+L["Set Scale"] = true
+L["Sets Scale of the Micro Bar"] = true
 L["As Letters"] = true
 L["Replace icons with just letters.\n|cffFF0000Warning:|r this will disable original Blizzard's tooltips for microbar."] = true
